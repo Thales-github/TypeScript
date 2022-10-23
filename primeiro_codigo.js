ffd855d7303c82f4e3ff4426b76eb38edd634c56
Thales-github <thalesvitoroliveiracirino@gmail.com>
2022-10-23 20:42:27 -0300

    Primeiros passos no TypeScript

---
+++ primeiro_codigo.js
@@ -0,0 +1,11 @@
+console.log("typescript rodando...");
+console.log("====================");
+const nome = "Thales";
+console.log(`Olá ${nome} seja bem vindo`);
+class Produto {
+    constructor(produtoNome, produtoValor) {
+        this.nome = produtoNome;
+        this.valor = produtoValor;
+    }
+}
+const playStation5 = new Produto("PlayStation", "5000");
