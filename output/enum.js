a11b157ce11972f7e242ddc74a6cce5e50646464
Thales-github <thalesvitoroliveiracirino@gmail.com>
2022-11-06 15:32:15 -0300

    correções

---
+++ output/enum.js
@@ -0,0 +1,11 @@
+"use strict";
+var Permission;
+(function (Permission) {
+    Permission[Permission["ADMIN"] = 0] = "ADMIN";
+    Permission[Permission["USER"] = 1] = "USER";
+    Permission[Permission["READONLY"] = 2] = "READONLY";
+})(Permission || (Permission = {}));
+console.log(Permission.ADMIN);
+console.log(Permission.USER);
+console.log(Permission.READONLY);
+//# sourceMappingURL=enum.js.map
